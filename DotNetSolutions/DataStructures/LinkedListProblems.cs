705817fbdeb384d2dd77555059d453aac8b3d1fc
EX2520\Renat <renhair@gmail.com>
2022-06-08 22:48:57 +0300

    141. Linked List Cycle

---
+++ DotNetSolutions/DataStructures/LinkedListProblems.cs
@@ -0,0 +1,47 @@
+﻿namespace DotNetSolutions.DataStructures
+{
+    // Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode? next;
+        public ListNode(int x)
+        {
+            val = x;
+            next = null;
+        }
+    }
+
+    public static class LinkedListProblems
+    {
+        /// <summary>
+        /// 141. Linked List Cycle
+        /// There is a cycle in a linked list if there is some node in the list 
+        /// that can be reached again by continuously following the next pointer. 
+        /// Internally, pos is used to denote the index of the node that tail's next pointer 
+        /// is connected to. Note that pos is not passed as a parameter.
+        /// 
+        /// Return true if there is a cycle in the linked list.Otherwise, return false
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public static bool HasCycle(ListNode head)
+        {
+            if (head == null) return false;
+
+            ListNode? node1 = head;
+            ListNode? node2 = node1.next;
+
+            while (node1 != null && node2 != null)
+            {
+                if (node1 == node2)
+                    return true;
+
+                node1 = node1.next;
+                node2 = node2.next?.next;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ DotNetSolutionsTests/DataStructures/LinkedListTests.cs
@@ -0,0 +1,48 @@
+﻿using DotNetSolutions.DataStructures;
+using Xunit;
+
+namespace DotNetSolutionsTests.DataStructures
+{
+    public class LinkedListTests
+    {
+        [Fact]
+        public void HasCycleTest1()
+        {
+            var n0 = new ListNode(3);
+            var n1 = new ListNode(2);
+            var n2 = new ListNode(0);
+            var n3 = new ListNode(-4);
+            n0.next = n1;
+            n1.next = n2;
+            n2.next = n3;
+            n3.next = n1;
+
+            var result = LinkedListProblems.HasCycle(n0);
+            
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void HasCycleTest2()
+        {
+            var n0 = new ListNode(1);
+            var n1 = new ListNode(2);
+            n0.next = n1;
+            n1.next = n0;
+
+            var result = LinkedListProblems.HasCycle(n0);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void HasCycleTest3()
+        {
+            var n0 = new ListNode(1);
+
+            var result = LinkedListProblems.HasCycle(n0);
+
+            Assert.False(result);
+        }
+    }
+}
