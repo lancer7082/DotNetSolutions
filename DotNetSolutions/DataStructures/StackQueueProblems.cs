44e859af4f801f7644b73543bf6e0d5797c736e2
EX2520\Renat <renhair@gmail.com>
2022-06-19 22:15:32 +0300

    20. Valid Parentheses

---
+++ DotNetSolutions/DataStructures/StackQueueProblems.cs
@@ -0,0 +1,44 @@
+﻿namespace DotNetSolutions.DataStructures
+{
+    public static class StackQueueProblems
+    {
+        /// <summary>
+        /// 20. Valid Parentheses
+        /// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', 
+        /// determine if the input string is valid.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool IsValidParentheses(string s)
+        {
+            string openParentheses = "({[";
+            string closeParentheses = ")}]";
+            var stack = new Stack<char>();
+            foreach(var c in s)
+            {
+                if (openParentheses.Contains(c))
+                {
+                    stack.Push(c);
+                }
+                else if (closeParentheses.Contains(c))
+                {
+                    if (stack.Count == 0)
+                        return false;
+
+                    var c1 = stack.Pop();
+                    if ((c == ')' && c1 != '(')
+                        || (c == '}' && c1 != '{')
+                        || (c == ']' && c1 != '['))
+                    {
+                        return false;   
+                    }
+                }
+            }
+
+            if (stack.Count > 0)
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ DotNetSolutionsTests/DataStructures/StackQueueTests.cs
@@ -0,0 +1,25 @@
+﻿using DotNetSolutions.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DotNetSolutionsTests.DataStructures
+{
+    public class StackQueueTests
+    {
+        [Theory]
+        [InlineData("()", true)]
+        [InlineData("()[]{}", true)]
+        [InlineData("(]", false)]
+        [InlineData("}{", false)]
+        void ValidParenthesesTest(string s, bool expectedResult)
+        {
+            bool result = StackQueueProblems.IsValidParentheses(s);
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
