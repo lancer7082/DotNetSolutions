268df3dcfea0e890ea040492a60f86b406d760b4
EX2520\Renat <renhair@gmail.com>
2022-06-23 22:17:14 +0300

    Tree Traversal

---
+++ DotNetSolutions/DataStructures/TreeProblems.cs
@@ -0,0 +1,71 @@
+﻿namespace DotNetSolutions.DataStructures
+{
+    public static class TreeProblems
+    {
+        /// <summary>
+        /// 94. Binary Tree Inorder Traversal
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static IList<int> InorderTraversal(TreeNode root)
+        {
+            var list = new List<int>();
+
+            void InorderTraversalRecursive(TreeNode? root)
+            {
+                if (root == null) return;
+                InorderTraversalRecursive(root.left);
+                list.Add(root.val);
+                InorderTraversalRecursive(root.right);
+            }
+
+            InorderTraversalRecursive(root);
+
+            return list;
+        }
+
+        /// <summary>
+        /// 145. Binary Tree Postorder Traversal
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static IList<int> PostorderTraversal(TreeNode root)
+        {
+            var list = new List<int>();
+
+            void PostorderTraversalRecursive(TreeNode? root)
+            {
+                if (root == null) return;
+                PostorderTraversalRecursive(root.left);
+                PostorderTraversalRecursive(root.right);
+                list.Add(root.val);
+            }
+
+            PostorderTraversalRecursive(root);
+
+            return list;
+        }
+
+        /// <summary>
+        /// 144. Binary Tree Preorder Traversal
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static IList<int> PreorderTraversal(TreeNode root)
+        {
+            var list = new List<int>();
+
+            void PreorderTraversalRecursive(TreeNode? root)
+            {
+                if (root == null) return;
+                list.Add(root.val);
+                PreorderTraversalRecursive(root.left);
+                PreorderTraversalRecursive(root.right);
+            }
+
+            PreorderTraversalRecursive(root);
+
+            return list;
+        }
+    }
+}
---
+++ DotNetSolutionsTests/DataStructures/TreeTests.cs
@@ -0,0 +1,51 @@
+﻿using DotNetSolutions.DataStructures;
+using System.Collections.Generic;
+using Xunit;
+
+namespace DotNetSolutionsTests.DataStructures
+{
+    public class TreeTests
+    {
+        [Fact]
+        public void InorderTraversalTest1()
+        {
+            var n3 = new TreeNode(3);
+            var n2 = new TreeNode(2, n3);
+            var n1 = new TreeNode(1, right: n2);
+
+            var expectedResult = new List<int> { 1, 3, 2};
+
+            var result = TreeProblems.InorderTraversal(n1);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void PreorderTraversalTest1()
+        {
+            var n3 = new TreeNode(3);
+            var n2 = new TreeNode(2, n3);
+            var n1 = new TreeNode(1, right: n2);
+
+            var expectedResult = new List<int> { 1, 2, 3 };
+
+            var result = TreeProblems.PreorderTraversal(n1);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void PostorderTraversalTest1()
+        {
+            var n3 = new TreeNode(3);
+            var n2 = new TreeNode(2, n3);
+            var n1 = new TreeNode(1, right: n2);
+
+            var expectedResult = new List<int> { 3, 2, 1 };
+
+            var result = TreeProblems.PostorderTraversal(n1);
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
